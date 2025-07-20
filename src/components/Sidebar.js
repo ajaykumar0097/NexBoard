4727eef0e32943fc0f89bcf24adcffcf2ea88e19
Ajay Kumar <omkashyap9536@gmail.com>
2025-07-20 18:50:36 +0530

    add submenues in sidebar
    Signed-off-by: Ajay Kumar <omkashyap9536@gmail.com>

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { RxDashboard } from "react-icons/rx";
 import { FaAngleRight } from "react-icons/fa6";
@@ -12,14 +12,24 @@ import { IoMdNotificationsOutline } from "react-icons/io";
 import { IoSettingsOutline } from "react-icons/io5";
 import { TbPageBreak } from "react-icons/tb";
 import { Link } from "react-router-dom";
+import { AiOutlineLogout } from "react-icons/ai";
 const Sidebar = () => {
+  const [activeTab, setActiveTab] = useState(0);
+  const [isToggle, setIsToggle] = useState(false);
+
+  const isSubmenuOpen = (index) => {
+    setActiveTab(index);
+    setIsToggle(!isToggle);
+  };
   return (
     <>
       <div className="sidebar">
         <ul className="sidebar__list py-2 px-3  ">
           <li>
-         
-            <Button className=" w-100">
+            <Button
+              className={`w-100 ${activeTab === 0 && isToggle ? "active" : ""}`}
+              onClick={() => isSubmenuOpen(0)}
+            >
               <span className="icon">
                 <RxDashboard />
               </span>
@@ -28,16 +38,30 @@ const Sidebar = () => {
                 <FaAngleRight />
               </span>
             </Button>
-            <ul className="submenu">
-                <li><Link to='#'>Product List</Link></li>
-                <li><Link to='#'>Product View</Link></li>
-                <li><Link to='#'>Product Upload</Link></li>
-            </ul>
-  
+            <div
+              className={`submenuWrapper ${
+                activeTab === 0 && isToggle ? "colapse" : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Ecommerce</Link>
+                </li>
+                <li>
+                  <Link to="#">Analytics</Link>
+                </li>
+                <li>
+                  <Link to="#">CRM</Link>
+                </li>
+              </ul>
+            </div>
           </li>
 
-          <li>  
-            <Button className=" w-100">
+          <li>
+            <Button
+              className={`w-100 ${activeTab === 1 && isToggle ? "active" : ""}`}
+              onClick={() => isSubmenuOpen(1)}
+            >
               <span className="icon">
                 <IoMdLock />
               </span>
@@ -46,10 +70,30 @@ const Sidebar = () => {
                 <FaAngleRight />
               </span>
             </Button>
+            <div
+              className={`submenuWrapper ${
+                activeTab === 1 && isToggle ? "colapse" : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Loging</Link>
+                </li>
+                <li>
+                  <Link to="#">Registration</Link>
+                </li>
+                <li>
+                  <Link to="#">Forgot Password</Link>
+                </li>
+              </ul>
+            </div>
           </li>
 
-          <li>  
-            <Button className=" w-100">
+          <li>
+            <Button
+              className={`w-100 ${activeTab === 2 && isToggle ? "active" : ""}`}
+              onClick={() => isSubmenuOpen(2)}
+            >
               <span className="icon">
                 <FaRegCircleUser />
               </span>
@@ -57,11 +101,32 @@ const Sidebar = () => {
               <span className="arrow">
                 <FaAngleRight />
               </span>
-            </Button> 
+            </Button>
+
+            <div
+              className={`submenuWrapper ${
+                activeTab === 2 && isToggle ? "colapse" : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">User List</Link>
+                </li>
+                <li>
+                  <Link to="#">User Profile</Link>
+                </li>
+                <li>
+                  <Link to="#">My Account</Link>
+                </li>
+              </ul>
+            </div>
           </li>
 
-          <li>  
-            <Button className=" w-100">
+          <li>
+            <Button
+              className={`w-100 ${activeTab === 3 && isToggle ? "active" : ""}`}
+              onClick={() => isSubmenuOpen(3)}
+            >
               <span className="icon">
                 <AiOutlineProduct />
               </span>
@@ -69,11 +134,32 @@ const Sidebar = () => {
               <span className="arrow">
                 <FaAngleRight />
               </span>
-            </Button> 
+            </Button>
+
+            <div
+              className={`submenuWrapper ${
+                activeTab === 3 && isToggle ? "colapse" : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Product List</Link>
+                </li>
+                <li>
+                  <Link to="#">Product View</Link>
+                </li>
+                <li>
+                  <Link to="#">Product Upload</Link>
+                </li>
+              </ul>
+            </div>
           </li>
 
-          <li>  
-            <Button className=" w-100">
+          <li>
+            <Button
+              className={`w-100 ${activeTab === 4 && isToggle ? "active" : ""}`}
+              onClick={() => isSubmenuOpen(4)}
+            >
               <span className="icon">
                 <TbFileInvoice />
               </span>
@@ -81,164 +167,108 @@ const Sidebar = () => {
               <span className="arrow">
                 <FaAngleRight />
               </span>
-            </Button> 
+            </Button>
+
+            <div
+              className={`submenuWrapper ${
+                activeTab === 4 && isToggle ? "colapse" : "colapsed"
+              }`}
+            >
+              <ul className="submenu">
+                <li>
+                  <Link to="#">Invoice List</Link>
+                </li>
+                <li>
+                  <Link to="#">Invoice Details</Link>
+                </li>
+              </ul>
+            </div>
           </li>
 
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <MdOutlineShoppingCart />
-              </span>
-              Orders
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
+          <li>
+            {" "}
+            <Link>
+              <Button
+                className={`w-100 ${activeTab === 5 ? "active" : ""}`}
+                onClick={() => isSubmenuOpen(5)}
+              >
+                <span className="icon">
+                  <MdOutlineShoppingCart />
+                </span>
+                Orders
+              </Button>{" "}
+            </Link>
           </li>
 
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <FaRegEnvelope />
-              </span>
-              Mesages
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
+          <li>
+            {" "}
+            <Link>
+              <Button
+                className={`w-100 ${activeTab === 6 ? "active" : ""}`}
+                onClick={() => isSubmenuOpen(6)}
+              >
+                <span className="icon">
+                  <FaRegEnvelope />
+                </span>
+                Mesages
+              </Button>{" "}
+            </Link>
           </li>
 
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoMdNotificationsOutline />
-              </span>
-              Notifications
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
+          <li>
+            {" "}
+            <Link>
+              <Button
+                className={`w-100 ${activeTab === 7 ? "active" : ""}`}
+                onClick={() => isSubmenuOpen(7)}
+              >
+                <span className="icon">
+                  <IoMdNotificationsOutline />
+                </span>
+                Notifications
+              </Button>{" "}
+            </Link>
           </li>
 
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoSettingsOutline />
-              </span>
-              Settings
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
+          <li>
+            {" "}
+            <Link>
+              <Button
+                className={`w-100 ${activeTab === 8 ? "active" : ""}`}
+                onClick={() => isSubmenuOpen(8)}
+              >
+                <span className="icon">
+                  <IoSettingsOutline />
+                </span>
+                Settings
+              </Button>{" "}
+            </Link>
           </li>
 
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <TbPageBreak />
-              </span>
-              Blank Page
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <FaRegEnvelope />
-              </span>
-              Mesages
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoMdNotificationsOutline />
-              </span>
-              Notifications
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoSettingsOutline />
-              </span>
-              Settings
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <TbPageBreak />
-              </span>
-              Blank Page
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <FaRegEnvelope />
-              </span>
-              Mesages
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoMdNotificationsOutline />
-              </span>
-              Notifications
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <IoSettingsOutline />
-              </span>
-              Settings
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
-          </li>
-
-          <li>  <Link>
-            <Button className=" w-100">
-              <span className="icon">
-                <TbPageBreak />
-              </span>
-              Blank Page
-              <span className="arrow">
-                <FaAngleRight />
-              </span>
-            </Button> </Link>
+          <li>
+            {" "}
+            <Link>
+              <Button
+                className={`w-100 ${activeTab === 9 ? "active" : ""}`}
+                onClick={() => isSubmenuOpen(9)}
+              >
+                <span className="icon">
+                  <TbPageBreak />
+                </span>
+                Blank Page
+              </Button>{" "}
+            </Link>
           </li>
         </ul>
+
+        <div className="logoutWrapper">
+          <div className="logoutBox">
+            <Button variant="contained">
+              <AiOutlineLogout className="m-1" />
+              Logout
+            </Button>
+          </div>
+        </div>
       </div>
     </>
   );
